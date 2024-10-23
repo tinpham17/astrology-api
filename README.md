3b2e3868cbd9152a8afd76105a4b33c3cb816881
Tin Pham <7inpham@gmail.com>
2024-10-23 23:38:21 +0700

    setup db connection

---
+++ README.md
@@ -0,0 +1,26 @@
+# Astrology API
+
+## Run locally
+
+- Run Postgres in Docker:
+
+```
+docker run --name astrology-postgres -p 5432:5432 -e POSTGRES_USER=postgres-user -e POSTGRES_PASSWORD=postgres-pw -e POSTGRES_DB=astrology-db -d postgres
+```
+
+- Run server:
+
+```
+npm run dev
+```
+
+## Run in production
+
+```
+npm run build
+npm start
+```
+
+## Extensions
+
+- Prisma
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { prisma } from './db';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api', async (req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+})
 
 app.listen(3000, () => {
   console.log('Astrology API running on port 3000');
